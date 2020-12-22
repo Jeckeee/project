1a7e4dfab6ed2bacc7261fcf499d6bcc4126717f
Evgeniy <metkiy23@mail.ru>
2020-12-22 16:14:27 +0700

    del hw-1

--- homework01/task_1.1.py
+++
@@ -1,7 +0,0 @@
-variable_01 = input("Введите слово >>> ")
-variable_02 = input("Введите число >>> ")
-variable_03 = f"Ваше слово <{variable_01}> и число <{variable_02}>!"
-
-print(variable_01)
-print(variable_02)
-print(variable_03)
